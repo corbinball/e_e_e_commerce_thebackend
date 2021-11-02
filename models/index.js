10ca92d2a7c6c32ee9640a7bad969f911af72e73
Corbin Ball <c.ball@me.com>
2021-11-01 23:05:44 -0500

    added association that products belongtomany tags through producttag

--- models/index.js
+++ models/index.js
@@ -15,8 +15,13 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { 
+  through: 'ProductTag', 
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+
 
 module.exports = {
   Product,
