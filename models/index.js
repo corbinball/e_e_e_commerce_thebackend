471103bac73bf25b120592ffc777db3b29cea477
Corbin Ball <c.ball@me.com>
2021-11-05 13:13:47 -0500

    changes with help from BCS

--- models/index.js
+++ models/index.js
@@ -11,19 +11,17 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { 
-  through: 'ProductTag', 
-  foreignKey: 'product_id',
+  through: {model: ProductTag }
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { 
-  through: 'ProductTag', 
-  foreignKey: 'tag_id',
+  through: {model: ProductTag}
 });
 
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}],
+      include: [ 
+        { model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}
+      ],
     });
     res.status(200).json(categoryData);
   } catch (err) {
