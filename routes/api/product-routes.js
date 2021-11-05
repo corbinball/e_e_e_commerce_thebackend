8d5419168ac7bb7f6a2d2f2d690489f34c9b874a
Corbin Ball <c.ball@me.com>
2021-11-05 10:15:16 -0500

    updates

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/api/products', (req, res) => {
+router.get('/api/products', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -13,7 +13,13 @@ router.get('/api/products', (req, res) => {
                 {model: Tag }
               ],
     });
+    
+    console.log("productsData");
+
     res.status(200).json(productsData);
+
+    console.log("productsData");
+
   } catch (err) {
     res.status(500).json(err);
   }
